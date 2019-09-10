3e1dfb61a8f35276c7cf8e39be34a2db3e7852e8
Ke1es7iaN <54950776+Ke1es7iaN@users.noreply.github.com>
2019-09-10 21:20:21 +0500

    Add files via upload

---
+++ Self powers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Self_powers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine(SummarizeSelfPowers(int.Parse(Console.ReadLine())));
+            }
+        }
+
+        static long SummarizeSelfPowers(int n)
+        {
+            var result = 0l;
+            for (int i = 1; i <= n; i++)
+            {
+                var product = (long)i;
+                for (int j = 2; j <= i; j++)
+                {
+                    product = (product * i) ;
+                }
+                result = (result + product) % 10000000000;
+            }
+            return result;
+        }
+    }
+}
